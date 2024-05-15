c9068a7939d0a045b6d92b05ae10df866430e859
RMN30654 <rahmatullahmasumnobin.rmn30654@gmail.com>
2024-05-15 17:51:23 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-apt-get install python3 && python3-opencv && python-tk
+apt-get install python3 && apt-get install python3-opencv && apt-get install python-tk
 python3 startupappset.py
-cp python3.desktop ~/.config/autostart/
+cp python3.desktop ~/.config/autostart
 
