6e990a2c51483b10700638e254feb6b14715fe0a
RMN30654 <rahmatullahmasumnobin.rmn30654@gmail.com>
2024-05-15 17:58:52 +0600

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 apt-get install python3 && apt-get install python3-opencv && apt-get install python-tk
 python3 startupappset.py
 cp python3.desktop ~/.config/autostart
-
+echo 0 > ./alQuran/tracker.txt
